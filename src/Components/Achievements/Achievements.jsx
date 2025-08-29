ba3bdb38e077f557cbae86f12a00817cd005073c
Anmol840 <ag6939541@gmail.com>
2025-08-29 08:58:19 +0530

    Update Achievements.jsx

--- src/Components/Achievements/Achievements.jsx
+++ src/Components/Achievements/Achievements.jsx
@@ -4,49 +4,25 @@ import "@splidejs/react-splide/css";
 import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
 import "./Achievements.css";
 import MoviesCarousel from "../../Components/MoviesCarousel/MoviesCarousel";
+import Carousel from "../../Components/Carousel/Carousel.jsx";
+// Assets
+import slideImg1 from "../../assets/slide-icon-1.svg";
+import slideImg2 from "../../assets/slide-icon-2.svg";
+import slideImg3 from "../../assets/slide-icon-3.svg";
+
+import slideImg4 from "../../assets/slide-icon-4.svg";
+import slideImg5 from "../../assets/slide-icon-5.svg";
 
 const Achievements = () => {
   return (
     <>
-      <div
-        id="ACHIEVEMENTS"
-        className="relative text-center w-full py-[10px] pt-0 mt-[80px]"
-        
-      >
-        <Splide
-          options={{
-            type: "loop",
-            drag: false,
-            arrows: false,
-            pagination: false,
-            autoScroll: {
-              speed: 1.5,
-              pauseOnHover: true,
-              pauseOnFocus: true,
-            },
-          }}
-          extensions={{ AutoScroll }}
-          className="w-full"
-          aria-label="Smooth Scrolling Work Slider"
-        >
-          <SplideSlide>
-            <div className="section-title">ACHIEVEMENTS</div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section-title">ACHIEVEMENTS</div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section-title">ACHIEVEMENTS</div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section-title">ACHIEVEMENTS</div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="section-title">ACHIEVEMENTS</div>
-          </SplideSlide>
-        </Splide>
-      </div>
-
+    <section className="py-10">
+        <Carousel
+          slides={[slideImg1, slideImg2, slideImg3]}
+          autoplay={true}
+          interval={3000}
+        />
+      </section>
       <section className="py-10">
         <MoviesCarousel autoplay={true} interval={2500} />
       </section>
@@ -55,3 +31,4 @@ const Achievements = () => {
 };
 
 export default Achievements;
+
